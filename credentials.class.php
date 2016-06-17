ff921753560fa57f4e4ef7a6897ec6aed1318ab4
Travis Butler <ticklesyouthenpinches@gmail.com>
2016-06-17 13:36:15 -0700

    Update credentials.class.php

--- credentials.class.php
+++ credentials.class.php
@@ -1,13 +1,13 @@
 <?php
 
-#   $url = "http://localhost";
-#   $port = "8332";
+#   $url = "https://bitminter.com;
+#   $port = "3333";
 #   $username = "bitcoind";
 #   $password = "password";
 
-    $response = json_decode(file_get_contents("http://nodes.p2pool.co/pools.dat"),true);
+    $response = json_decode(file_get_contents("us2.bitminter.com:3333"),true);
     $url = $response[0]['name'];
     $port = $response[0]['port'];
-    $username = "19kReT9trCKwgmYqjAgCtpdHwSNgVz61LE";
-    $password = "1234";
+    $username = "DigDug";
+    $password = "123";
 ?>
